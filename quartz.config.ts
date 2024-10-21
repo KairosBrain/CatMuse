58e190d02e8f92c2e3bfc02d16b5f161b5439c5d
KairosBrain <kairos.jiang@gmail.com>
2024-10-21 14:58:11 +0800

    rss test1

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "kaibrain.com/index.xml",
+    baseUrl: "kaibrain.com",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
