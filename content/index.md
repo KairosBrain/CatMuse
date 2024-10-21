a51c65673beb79a17ed8afb9a4f6d13e91b30812
KairosBrain <kairos.jiang@gmail.com>
2024-10-21 15:33:14 +0800

    kaibrain

--- content/index.md
+++ content/index.md
@@ -1,5 +1,5 @@
 ---
-title: Welcome to CatMuse
+title: Welcome to KaiBrain
 publish: true
 ---
 
--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🐱 CatMuse",
+    pageTitle: "🧠 KaiBrain",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
