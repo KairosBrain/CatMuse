d3aa9368120dadeed02329a05f378a212dd721d7
KairosBrain <kairos.jiang@gmail.com>
2024-10-21 14:47:23 +0800

    rss test

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "kaibrain.com",
+    baseUrl: "kaibrain.com/index.xml",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
